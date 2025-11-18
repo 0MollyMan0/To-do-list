12453d14efd1d761738cfe4f7cd1c4c45ba9fb5d
0MollyMan0 <fougereantoine0pro@gmail.com>
2025-11-18 09:16:44 +0100

    fruit form restructuration whith good practice

--- src/App.jsx
+++ src/App.jsx
@@ -3,35 +3,25 @@ import Fruit from "./components/Fruit";
 import FruitForm from "./components/FruitForm";
 
 function App() {
-  // state
+  
   const [fruits, setFruits] = useState([
     { id: 1, nom: "Abricots" },
     { id: 2, nom: "Bananes" },
     { id: 3, nom: "Cerises" },
   ]);
 
-  const [newFruit, setNewFruit] = useState("");
-
-  // comportements
   const handleDelete = (id) => {
     const fruitsCopy = [...fruits];
     const fruitsCopyUpdated = fruitsCopy.filter((fruit) => fruit.id !== id);
     setFruits(fruitsCopyUpdated);
   };
 
-  const handleChange = (event) => {
-    setNewFruit(event.target.value);
-  };
+  const handleAdd = (addFruit) => {
+      const fruitsCopy = [...fruits];
+      fruitsCopy.push(addFruit);
+      setFruits(fruitsCopy);
+  }
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const fruitsCopy = [...fruits];
-    const addFruit = { id: new Date().getTime(), nom: newFruit };
-    fruitsCopy.push(addFruit);
-    setFruits(fruitsCopy);
-    setNewFruit("");
-  };
-  // affichage
   return (
     <div>
       <h1>Liste de fruits</h1>
@@ -40,7 +30,7 @@ function App() {
           return <Fruit fruitInfo={fruit} onFruitDelete={handleDelete} />;
         })}
       </ul>
-      <FruitForm newFruit={newFruit} onFruitSubmit={handleSubmit} onFruitChange={handleChange}/>
+      <FruitForm handleAdd={handleAdd}/>
     </div>
   );
 }
--- src/components/FruitForm.jsx
+++ src/components/FruitForm.jsx
@@ -1,11 +1,26 @@
-export default function FruitForm({newFruit, onFruitSubmit, onFruitChange}) {
-  return (
-        <form action="submit" onSubmit={onFruitSubmit}>
+import { useState } from "react";
+
+export default function FruitForm({handleAdd}) {
+    const [newFruit, setNewFruit] = useState("");
+  
+    const handleChange = (event) => {
+      setNewFruit(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const addFruit = { id: new Date().getTime(), nom: newFruit };
+      handleAdd(addFruit);
+      setNewFruit("");
+    };
+
+    return (
+        <form action="submit" onSubmit={handleSubmit}>
         <input
           value={newFruit}
           type="text"
           placeholder="Ajouter un fruit..."
-          onChange={onFruitChange}
+          onChange={handleChange}
         />
         <button>Ajouter</button>
       </form>
